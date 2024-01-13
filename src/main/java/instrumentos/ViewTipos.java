6cb4b4554908c38b3d1087094f345bbea6057f00
MacoGitHub <macochaves13@gmail.com>
2024-01-13 17:19:59 -0600

    View de Tipos

--- src/main/java/instrumentos/ViewTipos.java
+++ src/main/java/instrumentos/ViewTipos.java
@@ -1,10 +1,5 @@
 package instrumentos;
-import javax.swing.JPanel;
-import javax.swing.JLabel;
-import javax.swing.JTextField;
-import javax.swing.JButton;
-import javax.swing.JTable;
-import javax.swing.JOptionPane;
+import javax.swing.*;
 import javax.swing.table.TableColumnModel;
 import java.awt.Desktop;
 import java.awt.Color;
